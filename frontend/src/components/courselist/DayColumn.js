04954ac51943fb1eb8993906c50c72a95fa76f40
Cheyenne Zhang <cz10@princeton.edu>
2021-01-25 16:57:50 -0500

    test

---
+++ frontend/src/components/courselist/DayColumn.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import $ from 'jquery';
+import { ajaxSetup } from 'AjaxSetup';
+import { ThemeProvider } from 'styled-components';
+import { COURSESELECTION_THEME } from 'styles/theme';
+import '../../styles/CourseList.css';
+const DayColumn = (props) => {
+  ajaxSetup();
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+  console.log('Displaying Course', props.course);
+
+  // Example API request
+  const fetchProfile = () => {
+    $.ajax({
+      url: '/api/v1/get_profile/',
+      datatype: 'json',
+      type: 'GET',
+      success: (profile) => setProfile({ profile }),
+    });
+  };
+
+  return (
+    <div className="details-column">
+      <CourseHeader course={props.course} rating = {4.5} />
+      <div className="row">
+        <div className="scrollable-column">
+          <p className="section-header"> Ratings </p>
+          <Ratings  rating={3.87} />
+          <Ratings  rating={5.0} />
+          <Ratings  rating={4.5} />
+          <Ratings  rating={3.2} />
+          <p className="section-header"> Past Semesters </p>
+        </div>
+        <div className="scrollable-column">
+          <p> column 2 </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DayColumn;
